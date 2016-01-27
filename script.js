d0916413536ba8b4cb4e26efbafc0799cdbc9b77
Katie Fritz <katie@katiemfritz.com>
2016-01-27 11:29:09 -0500

    random selection alert works

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var todos = []
 var newTodoInput = document.getElementById('new-todo-input')
 var todoList = document.getElementById('todo-list')
 var todoCount = document.getElementById('todo-count')
+var cantDecide = document.getElementById('cant-decide')
 
 // Renders
 
@@ -17,7 +18,16 @@ function renderTodos() {
 }
 
 function countTodos() {
+  if (todos.length === 0) {
+    return todoCount.innerHTML = ''
+  }
   todoCount.innerHTML = '<strong>Items:</strong> ' + todos.length + ' <button class="clear-all">Clear All</button>'
+}
+
+function pickForMe() {
+  if (todos.length > 0) {
+      cantDecide.innerHTML = '<button class="cant-decide">Can\'t Decide</button>'
+  }
 }
 
 // Events
@@ -28,6 +38,7 @@ newTodoInput.onkeypress = function(event) {
     this.value = ''
     renderTodos()
     countTodos()
+    pickForMe()
   }
 }
 
@@ -42,9 +53,16 @@ todoList.onclick = function(event) {
 todoCount.onclick = function(event) {
   var clickedElement = event.target
   if (clickedElement.className === 'clear-all') {
-    console.log('You cleared all')
     todos.splice(0)
     renderTodos()
     countTodos()
+  }
+}
+
+cantDecide.onclick = function(event) {
+  var clickedElement = event.target
+  if (clickedElement.className === "cant-decide") {
+    var suggestion = todos[Math.floor(Math.random() * todos.length)];
+    alert("Do this: " + suggestion)
   }
 }
