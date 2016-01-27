ad07c5d656aeb7421263cae465945a9e27b6c94a
Katie Fritz <katie@katiemfritz.com>
2016-01-27 11:44:06 -0500

    refactor

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ var todoList = document.getElementById('todo-list')
 var todoCount = document.getElementById('todo-count')
 var cantDecide = document.getElementById('cant-decide')
 
-// Renders
+// Renderers
+
+function renderAll() {
+  renderTodos()
+  countTodos()
+  pickForMe()
+}
 
 function renderTodos() {
   todoList.innerHTML = todos.map(function(todo, index) {
     return '<li>' +
       todo +
+      ' <button class="move-up" data-index="' + index + '">↑</button>' +
+      ' <button class="move-down" data-index="' + index + '">↓</button>' +
       ' <button class="remove-todo" data-index="' + index + '">X</button>' +
     '</li>'
   }).join('')
@@ -25,40 +33,52 @@ function countTodos() {
 }
 
 function pickForMe() {
-  if (todos.length > 0) {
-      cantDecide.innerHTML = '<button class="cant-decide">Can\'t Decide</button>'
+  if (todos.length === 0) {
+    return cantDecide.innerHTML = "Add some items and we'll suggest one for you."
   }
+  cantDecide.innerHTML = '<button class="cant-decide">Can\'t Decide</button>'
 }
 
+// ******************************
 // Events
+// *******************************
 
+// add new todo
 newTodoInput.onkeypress = function(event) {
   if (event.which === 13) {
     todos.push(this.value)
     this.value = ''
-    renderTodos()
-    countTodos()
-    pickForMe()
+    renderAll()
   }
 }
 
+// move up
+todoList.onclick = function(event) {
+  var clickedElement = event.target
+  if (clickedElement.className === 'move-up') {
+    console.log('move up')
+  }
+}
+
+// remove todo
 todoList.onclick = function(event) {
   var clickedElement = event.target
   if (clickedElement.className === 'remove-todo') {
     todos.splice(clickedElement.dataset.index, 1)
-    renderTodos()
+    renderAll()
   }
 }
 
+// clear all
 todoCount.onclick = function(event) {
   var clickedElement = event.target
   if (clickedElement.className === 'clear-all') {
     todos.splice(0)
-    renderTodos()
-    countTodos()
+    renderAll()
   }
 }
 
+// can't decide
 cantDecide.onclick = function(event) {
   var clickedElement = event.target
   if (clickedElement.className === "cant-decide") {
